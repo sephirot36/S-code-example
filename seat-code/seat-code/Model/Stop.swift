16945b7e6c975f17c6797b02755b953551eeb360
Daniel Castro muñoz <sephirot36@gmail.com>
2020-02-02 12:21:40 +0100

    Created models

---
+++ seat-code/seat-code/Model/Stop.swift
@@ -0,0 +1,12 @@
+//
+//  Stop.swift
+//  seat-code
+//
+//  Created by Daniel Castro muñoz on 02/02/2020.
+//  Copyright © 2020 Sephirot36. All rights reserved.
+//
+
+struct Stop {
+    var id: Int
+    var point: MapPoint
+}
---
+++ seat-code/seat-code/Model/Trip.swift
@@ -0,0 +1,28 @@
+//
+//  Trip.swift
+//  seat-code
+//
+//  Created by Daniel Castro muñoz on 02/02/2020.
+//  Copyright © 2020 Sephirot36. All rights reserved.
+//
+import CoreLocation
+
+enum RouteStatus {
+    case ongoing
+    case scheduled
+    case finalized
+    case canceled
+}
+
+struct Trip {
+    var description: String
+    var driverName: String
+    var startTime: String
+    var endTime: String
+    var routeString: String
+    var routeCoords: [CLLocationCoordinate2D]
+    var status: RouteStatus
+    var stops: [Stop]
+    var origin: MapPoint
+    var destination: MapPoint
+}
