6a42c8b332780021f665c5782489c7f5fba147e4
Daniel Castro muñoz <sephirot36@gmail.com>
2020-02-02 12:27:25 +0100

    Modified models

--- seat-code/seat-code/Model/Point.swift
+++ seat-code/seat-code/Model/Point.swift
@@ -1,12 +1,12 @@
 //
-//  MapPoint.swift
+//  Point.swift
 //  seat-code
 //
 //  Created by Daniel Castro muñoz on 02/02/2020.
 //  Copyright © 2020 Sephirot36. All rights reserved.
 //
 
-struct MapPoint {
-    var point: Point
-    var address: String = ""
+struct Point {
+    var latitude: Float
+    var longitude: Float
 }
