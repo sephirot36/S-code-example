d079fd0cd51c0fe46d3db471cefaceca9c2a2fd7
Daniel Castro <d.castro@whisbi.com>
2020-02-05 12:20:39 +0100

    Modified structure to control map in other class Created protocol to control map

--- seat-code/seat-code/View/MainViewController.swift
+++ seat-code/seat-code/View/MainViewController.swift
@@ -62,10 +62,10 @@ class MainViewController: UIViewController {
 
         self.tableView.rx.modelSelected(Trip.self)
             .subscribe(onNext: { [weak self] trip in
-                self?.loadRoute(routePoints: trip.routeCoords)
-                self?.createMarkerAt(point: trip.origin.point)
-                self?.createMarkerAt(point: trip.destination.point)
-                self?.displayStops(stops: trip.stops)
+//                self?.loadRoute(routePoints: trip.routeCoords)
+//                self?.createMarkerAt(point: trip.origin.point)
+//                self?.createMarkerAt(point: trip.destination.point)
+//                self?.displayStops(stops: trip.stops)
             }).disposed(by: self.disposeBag)
     }
 
@@ -96,39 +96,39 @@ class MainViewController: UIViewController {
     }
 
     // MARK: Public methods
-
-    public func loadRoute(routePoints: [CLLocationCoordinate2D]) {
-        self.mapView.clear()
-        let path = GMSMutablePath()
-        for point in routePoints {
-            path.add(point)
-        }
-
-        let route = GMSPolyline(path: path)
-        route.map = self.mapView
-
-        let bounds = GMSCoordinateBounds(coordinate: routePoints[0], coordinate: routePoints[routePoints.count - 1])
-
-        let update = GMSCameraUpdate.fit(bounds, with: UIEdgeInsets(top: 40, left: 30, bottom: 40, right: 30))
-        self.mapView.moveCamera(update)
-    }
-
-    public func createMarkerAt(point: Point, color: UIColor = .clear) {
-        let position = CLLocationCoordinate2D(latitude: CLLocationDegrees(point.latitude), longitude: CLLocationDegrees(point.longitude))
-        let marker = GMSMarker(position: position)
-
-        if color != .clear {
-            marker.icon = GMSMarker.markerImage(with: color)
-        }
-
-        marker.map = self.mapView
-    }
-
-    public func displayStops(stops: [Stop]) {
-        for stop in stops {
-            self.createMarkerAt(point: stop.point.point, color: .orange)
-        }
-    }
+//
+//    public func loadRoute(routePoints: [CLLocationCoordinate2D]) {
+//        self.mapView.clear()
+//        let path = GMSMutablePath()
+//        for point in routePoints {
+//            path.add(point)
+//        }
+//
+//        let route = GMSPolyline(path: path)
+//        route.map = self.mapView
+//
+//        let bounds = GMSCoordinateBounds(coordinate: routePoints[0], coordinate: routePoints[routePoints.count - 1])
+//
+//        let update = GMSCameraUpdate.fit(bounds, with: UIEdgeInsets(top: 40, left: 30, bottom: 40, right: 30))
+//        self.mapView.moveCamera(update)
+//    }
+//
+//    public func createMarkerAt(point: Point, color: UIColor = .clear) {
+//        let position = CLLocationCoordinate2D(latitude: CLLocationDegrees(point.latitude), longitude: CLLocationDegrees(point.longitude))
+//        let marker = GMSMarker(position: position)
+//
+//        if color != .clear {
+//            marker.icon = GMSMarker.markerImage(with: color)
+//        }
+//
+//        marker.map = self.mapView
+//    }
+//
+//    public func displayStops(stops: [Stop]) {
+//        for stop in stops {
+//            self.createMarkerAt(point: stop.point.point, color: .orange)
+//        }
+//    }
 }
 
 // Extend Controller to perform UITableViewDelegate
